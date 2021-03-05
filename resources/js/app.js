fd343177409275691d890db32d56e21e20ea28bb
Chrissle28 <christian.z.dev@gmail.com>
2021-03-05 11:22:52 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+Vue.component('burger', require('./components/Burger.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    mounted() {
+        require('./service/headline_animation.service');
+        require('./service/nav.service');
+        require('./service/mainscroll.service');
+    },
+});
---
+++ resources/js/service/headline_animation.service.js
@@ -0,0 +1,25 @@
+const updateHeadlines = () => {
+  let headlines = document.querySelectorAll(".h1");
+  headlines.forEach(headline => {
+    if (isInViewport(headline)) {
+      headline.classList.add("aw--animate");
+    } else {
+      headline.classList.remove("aw--animate");
+    }
+  });
+};
+
+function isInViewport(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return bounding.top < window.innerHeight * 0.7;
+}
+
+let main = document.querySelector("main");
+
+main.addEventListener("scroll", () => {
+  updateHeadlines();
+});
+
+setTimeout(function() {
+  updateHeadlines();
+}, 200);
