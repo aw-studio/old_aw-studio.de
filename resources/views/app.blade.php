28d87295761d83ed0bd4f5a978828444cb7de062
Torge Hansen <torge@aw-studio.de>
2021-03-09 15:58:16 +0100

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @include('partials.favicon')
+
     <link rel="stylesheet" href="/css/app.css">
     <x-styles />
 </head>
