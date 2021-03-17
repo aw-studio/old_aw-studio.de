c60b52b581280fa805c531ae200b06afc971a24f
Torge Hansen <torge@aw-studio.de>
2021-03-17 16:38:24 +0100

    nowrap on post tags

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -2,7 +2,7 @@
     <div class="relative">
         <div class="absolute">
             @if(count($post->tags) > 0)
-                <div class="absolute z-20 px-4 py-2 text-xs tracking-widest text-white uppercase bg-black rounded left-5 top-5">
+                <div class="absolute z-20 px-4 py-2 text-xs tracking-widest text-white uppercase bg-black rounded left-5 top-5 whitespace-nowrap">
                     {{$post->tags[0]->title}}
                 </div>
             @endif
