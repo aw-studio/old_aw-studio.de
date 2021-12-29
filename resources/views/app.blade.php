1d54e145fe1bb2fb7fc632d532e4908f8aeb1626
Torge Hansen <torge@aw-studio.de>
2021-12-29 10:28:46 +0100

    fixed favicon not showing on subpages

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @yield('meta')
 
     @include('partials.favicon')
+    @yield('meta')
 
     <link rel="stylesheet" href="/css/app.css">
     <x-styles />
