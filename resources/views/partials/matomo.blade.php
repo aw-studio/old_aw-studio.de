e192453687fdc631f1a36ea0bb638c3cf7d9ba23
Gerrit <gerrit@aw-studio.de>
2023-05-08 11:15:45 +0200

    matomo phishing fix

--- resources/views/partials/matomo.blade.php
+++ resources/views/partials/matomo.blade.php
@@ -6,7 +6,7 @@
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-      var u="//matomo.aw-studio.de/matomo/";
+      var u="//stats.aw-studio.de/";
       _paq.push(['setTrackerUrl', u+'matomo.php']);
       _paq.push(['setSiteId', '1']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
