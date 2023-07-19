217d538615edc2b5b794e21b887ee4a4f2ca5aa3
Torge Hansen <torge@aw-studio.de>
2023-07-19 13:59:51 +0200

    404 locale fix

--- resources/views/components/hreflang.blade.php
+++ resources/views/components/hreflang.blade.php
@@ -1,4 +1,6 @@
-@foreach ($locales as $locale)
-    <link rel="alternate" href="{{ Request::route()->translate($locale) }}"
-        hreflang="{{ $locale }}" />
-@endforeach
+@if(Request::route() != null)
+    @foreach ($locales as $locale)
+        <link rel="alternate" href="{{ Request::route()->translate($locale) }}"
+            hreflang="{{ $locale }}" />
+    @endforeach
+@endif
